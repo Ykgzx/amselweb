d7cf20e8f69dc2a27131545e0ece8fd50d2d2df4
Suphakit Ranphol <supakit0920@gmail.com>
2025-11-13 10:28:14 +0700

    Test commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import  Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       {/* ใช้ bg-slate-50 เป็นสีพื้นหลังหลัก (สีขาวนวลๆ) 
         เพื่อให้ Card ที่เป็นสีขาว (bg-white) ลอยเด่นขึ้นมา 
       */}
+      <Navbar></Navbar>
       <body className={`${inter.className} bg-slate-50`}>{children}</body>
     </html>
   );
