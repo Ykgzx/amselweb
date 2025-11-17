e794c2799c2fddaaf179287cd669085893c38784
Suphakit Ranphol <supakit0920@gmail.com>
2025-11-17 14:06:55 +0700

    Pelicoat

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['en', 'th'],       // ถูกต้อง
+  locales: ['en', 'th','jp'],       // ถูกต้อง
   defaultLocale: 'th',          // default เป็นไทย ✅
   localePrefix: 'always',       // ใส่ prefix ภาษาเสมอ ✅
 });
